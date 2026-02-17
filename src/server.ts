0cc78c94c17d01e1d74aef6675a67ffe0acb8c75
Nigmabprod <mu692018@outlook.fr>
2026-02-17 15:38:39 +0100

    Ajout de l'étudiant

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,13 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server log machin on http://localhost:${PORT}`);
 });
+
+const etudiants = [
+  { id: 1, nom: "Dupont", prenom: "Jean" },
+  { id: 2, nom: "Martin", prenom: "Sophie" },
+  { id: 3, nom: "Doe", prenom: "John" },
+];
+
+app.get('/api/data', (req, res) => {
+  res.json(etudiants);
+});
